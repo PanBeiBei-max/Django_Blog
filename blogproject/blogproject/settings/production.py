c6b51ffcaacfbe586d20ae412ea783818e98f3b4
panbeibei <1132432040@qq.com>
2021-12-14 01:58:55 +0800

    增加git忽略文件,修改之前的错误

--- blogproject/blogproject/settings/production.py
+++ blogproject/blogproject/settings/production.py
@@ -1,12 +1,12 @@
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["Django_Blog_SECRET_KET"]
+SECRET_KEY = os.environ["Django_Blog_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['120.55.73.31','0.0.0.0:8000']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
